630699fc170ecdeed04abd7037335b0f2c8b229b
alex-shcherbakov <90749411+alex-shcherbakov@users.noreply.github.com>
2023-10-04 15:33:14 +0300

    Add files via upload
    Done version without work report

---
+++ Lab2/src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Product product1 = new Product(1, "Product 1", 50.99);
+        Product product2 = new Product(2, "Product 2", 100.00);
+
+        Cart cart = new Cart();
+        cart.addProduct(product1);
+        cart.addProduct(product2);
+
+        Order order = new Order(1, cart.getProducts());
+
+        order.setStatus("Shipped");
+
+        String orderStatus = order.getStatus();
+        System.out.println("Order status: " + orderStatus);
+        String prod = product1.getName();
+        System.out.println("Info about product: " + prod);
+
+    }
+}
