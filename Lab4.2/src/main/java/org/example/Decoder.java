585b2c470f08e147cb1969a1f7b3e9ef444921a3
alex-shcherbakov <alex.shcherbakov.2@gmail.com>
2023-10-28 18:52:02 +0300

    Completed task

---
+++ Lab4.2/src/main/java/org/example/Decoder.java
@@ -0,0 +1,49 @@
+package org.example;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class Decoder {
+    public static String decodeVowels(String input) {
+        return input
+                .replace("1", "a")
+                .replace("2", "e")
+                .replace("3", "i")
+                .replace("4", "o")
+                .replace("5", "u");
+    }
+
+    public static String decodeConsonants(String input) {
+        StringBuilder decoded = new StringBuilder();
+        for (char c : input.toCharArray()) {
+            if (Character.isLetter(c)) {
+                char decodedChar = (char) (c - 1);
+                if (decodedChar == '`') {
+                    decodedChar = 'z';
+                }
+                decoded.append(decodedChar);
+            } else {
+                decoded.append(c);
+            }
+        }
+        return decoded.toString();
+    }
+
+    public static String decodeMessage(String message) {
+        StringBuilder decodedMessage = new StringBuilder();
+        Pattern pattern = Pattern.compile("\\w+");
+        Matcher matcher = pattern.matcher(message);
+
+        while (matcher.find()) {
+            String word = matcher.group();
+            if (word.contains("1") || word.contains("2") || word.contains("3") || word.contains("4") || word.contains("5")) {
+                decodedMessage.append(decodeVowels(word));
+            } else {
+                decodedMessage.append(decodeConsonants(word));
+            }
+            if (matcher.end() < message.length()) {
+                decodedMessage.append(message.charAt(matcher.end()));
+            }
+        }
+        return decodedMessage.toString();
+    }
+}
