b8a3e7f8b1c80fa4738a4616c06c439734546e3d
alex-shcherbakov <alex.shcherbakov.2@gmail.com>
2023-11-06 16:43:54 +0200

    Second version

--- Lab6/src/main/java/org/example/CinemaHall.java
+++ Lab6/src/main/java/org/example/CinemaHall.java
@@ -2,39 +2,42 @@ package org.example;
 
 public class CinemaHall {
     private int[][][] Halls;
+    private int numRows;
+    private int numSeatsPerRow;
 
     public CinemaHall(int numHalls, int numRows, int numSeatsPerRow) {
+        this.numRows = numRows;
+        this.numSeatsPerRow = numSeatsPerRow;
         Halls = new int[numHalls][numRows][numSeatsPerRow];
     }
 
     public void bookSeats(int hallNumber, int row, int[] seats) {
         for (int seat : seats) {
-            if (Halls[hallNumber][row][seat] == 1) {
-                System.out.println("Seat " + seat + " \n" +
-                        "in a row " + row + " of hall№ " + hallNumber + " already booked");
+            if (Halls[hallNumber][row - 1][seat - 1] == 1) {
+                System.out.println("Seat " + seat + " in row " + row + " of hall №" + hallNumber + " is already booked.");
             } else {
-                Halls[hallNumber][row][seat] = 1;
-                System.out.println("Seat " + seat + " in a row " + row + " of hall№ " + hallNumber + " successfully booked.");
+                Halls[hallNumber][row - 1][seat - 1] = 1;
+                System.out.println("Seat " + seat + " in row " + row + " of hall №" + hallNumber + " successfully booked.");
             }
         }
     }
 
     public void cancelBooking(int hallNumber, int row, int[] seats) {
         for (int seat : seats) {
-            if (Halls[hallNumber][row][seat] == 0) {
-                System.out.println("Seat " + seat + " in a row " + row + " of hall№ " + hallNumber + " already free");
+            if (Halls[hallNumber][row - 1][seat - 1] == 0) {
+                System.out.println("Seat " + seat + " in row " + row + " of hall №" + hallNumber + " is already free.");
             } else {
-                Halls[hallNumber][row][seat] = 0;
-                System.out.println("Cancellation of a reservation for a seat " + seat + "in a row " + row + " of hall№ "
-                        + hallNumber + " done successfully");
+                Halls[hallNumber][row - 1][seat - 1] = 0;
+                System.out.println("Cancellation of a reservation for seat " + seat + " in row " + row + " of hall №"
+                        + hallNumber + " is done successfully.");
             }
         }
     }
 
     public boolean checkAvailability(int hallNumber, int numSeats) {
-        for (int row = 0; row < Halls[hallNumber].length; row++) {
+        for (int row = 0; row < numRows; row++) {
             int consecutiveSeats = 0;
-            for (int seat = 0; seat < Halls[hallNumber][row].length; seat++) {
+            for (int seat = 0; seat < numSeatsPerRow; seat++) {
                 if (Halls[hallNumber][row][seat] == 0) {
                     consecutiveSeats++;
                     if (consecutiveSeats == numSeats) {
@@ -49,9 +52,9 @@ public class CinemaHall {
     }
 
     public void printSeatingArrangement(int hallNumber) {
-        System.out.println("Placement scheme for the hall " + hallNumber + ":");
-        for (int row = 0; row < Halls[hallNumber].length; row++) {
-            for (int seat = 0; seat < Halls[hallNumber][row].length; seat++) {
+        System.out.println("Seating arrangement for hall " + hallNumber + ":");
+        for (int row = 0; row < numRows; row++) {
+            for (int seat = 0; seat < numSeatsPerRow; seat++) {
                 if (Halls[hallNumber][row][seat] == 0) {
                     System.out.print("O ");
                 } else {
